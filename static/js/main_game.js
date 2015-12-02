6ffb088944b397ab71f3a64b09d95f39ca44cf49
TimelyToga <tblumberg39@gmail.com>
2015-12-02 01:35:45 -0500

    Circle moves around and moves more when clicked

--- static/js/main_game.js
+++ static/js/main_game.js
@@ -2,6 +2,25 @@
  * Created by Tim on 02/12/15.
  */
 
-$(document).click(function() {
-    alert("You just clicked");
-});
+function init() {
+    var stage = new createjs.Stage("demoCanvas");
+
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    circle.addEventListener("click", handleClick);
+    stage.addChild(circle);
+    stage.update();
+    createjs.Ticker.addEventListener("tick", handleTick);
+    function handleTick(event) {
+        circle.x += 1;
+        stage.update();
+    }
+}
+
+function handleClick(event){
+    event.target.x += 5;
+    event.target.y += 5;
+    stage.update();
+}
