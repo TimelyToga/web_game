ff81b7125de7bae80f3f6d660f8f1d51bdf9b6c5
TimelyToga <tblumberg39@gmail.com>
2015-12-02 02:17:59 -0500

    Displaying a animated sprite is done

--- static/js/main_game.js
+++ static/js/main_game.js
@@ -3,14 +3,41 @@
  */
 
 function init() {
-    var stage = new createjs.SpriteStage("canvasElementId", false, false);
+    // Set up SpriteStage
+    var stage = new createjs.SpriteStage("game_canvas", false, false);
     stage.updateViewport(800, 600);
-    var image = new createjs.Bitmap("/../img/survival_figure.png");
-    stage.addChild(image);
-    createjs.Ticker.addEventListener("tick", handleTick);
+//    var image = new createjs.Bitmap("/../img/survival_figure.png");
+//    stage.addChild(image);
 
+
+    // SpriteSheet Data
+    var data = {
+        images: ["/../../static/img/sprite.png"],
+        frames: {width: 56, height: 71},
+        animations: {   stand: [17, 20],
+            right_bump: {
+                frames: [21, 27],
+                next: "stand",
+                speed: 2 }
+        }
+    };
+
+    // SpriteSheet creation / adding sprites
+    var spriteSheet = new createjs.SpriteSheet(data);
+    var container = new createjs.SpriteContainer(spriteSheet);
+    var spriteInstance = new createjs.Sprite(spriteSheet);
+    spriteInstance.gotoAndPlay("right_bump");
+
+    container.addChild(spriteInstance);
+    container.x = 100;
+
+    // Add SpriteContainer to SpriteStage
+    stage.addChild(container);
+    stage.update();
+
+    // Game Tick
+    createjs.Ticker.addEventListener("tick", handleTick);
     function handleTick(event) {
-        image.x += 10;
         stage.update();
     }
 }
